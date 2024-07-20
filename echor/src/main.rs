b88c6c90d381532d0b209c86434e0511758a7dd1
Ryoei Shimaura <ryoeishimaura@Ryoeis-MacBook-Pro.local>
2024-07-20 12:55:19 +0900

    ch2: define arguments and options

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,24 @@
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
-    let _matches = Command::new("echor")
+    let matches = Command::new("echor")
         .version("0.1.0")
         .author("Ryoei Shimaura <tinytortoise.dev@gmail.com>")
         .about("Rust version of `echo`")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print newline")
+        )
         .get_matches();
+
+    println!("{:#?}", matches)
 }
