b2742f94481c007f75ff314b2d1e40ae21e18cae
Ryoei Shimaura <ryoeishimaura@Ryoeis-MacBook-Pro.local>
2024-07-20 15:58:15 +0900

    ch2: add testcase

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
 fn dies_no_args() {
@@ -11,10 +12,13 @@ fn dies_no_args() {
 }
 
 #[test]
-fn runs() {
+fn hello1() {
+  let outfile = "tests/expected/hello1.txt";
+  let expected = fs::read_to_string(outfile).unwrap();
   let mut cmd = Command::cargo_bin("echor").unwrap();
   cmd
-    .arg("hello")
+    .arg("Hello there")
     .assert()
-    .success();
+    .success()
+    .stdout(expected);
 }
