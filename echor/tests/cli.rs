c591294e2a129c77b49bb494379c45fe0b401a79
Ryoei Shimaura <ryoeishimaura@Ryoeis-MacBook-Pro.local>
2024-07-20 16:06:10 +0900

    ch2: change the way to write

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,24 +1,27 @@
+use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
 #[test]
-fn dies_no_args() {
-  let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> Result<()> {
+  let mut cmd = Command::cargo_bin("echor")?;
   cmd
     .assert()
     .failure()
     .stderr(predicate::str::contains("Usage"));
+  Ok(())
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> Result<()> {
   let outfile = "tests/expected/hello1.txt";
-  let expected = fs::read_to_string(outfile).unwrap();
-  let mut cmd = Command::cargo_bin("echor").unwrap();
+  let expected = fs::read_to_string(outfile)?;
+  let mut cmd = Command::cargo_bin("echor")?;
   cmd
     .arg("Hello there")
     .assert()
     .success()
     .stdout(expected);
+  Ok(())
 }
