92dd39a62f81d2c6e3d172bf409e9eec675e301a
Ryoei Shimaura <ryoeishimaura@Ryoeis-MacBook-Pro.local>
2024-07-20 12:42:24 +0900

    ch2: define first cli info

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Ryoei Shimaura <tinytortoise.dev@gmail.com>")
+        .about("Rust version of `echo`")
+        .get_matches();
 }
