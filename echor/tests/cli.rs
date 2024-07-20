47f4ce45854b4b63f071e712b69ed924709f2ad5
Ryoei Shimaura <ryoeishimaura@Ryoeis-MacBook-Pro.local>
2024-07-20 16:45:57 +0900

    ch2: add helper fn

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -3,6 +3,18 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+fn run(args: &[&str], expected_file: &str) -> Result<()> {
+  let expected = fs::read_to_string(expected_file)?;
+  let output = Command::cargo_bin("echor")?
+    .args(args)
+    .output()
+    .expect("fail");
+
+  let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+  assert_eq!(stdout, expected);
+  Ok(())
+}
+
 #[test]
 fn dies_no_args() -> Result<()> {
   let mut cmd = Command::cargo_bin("echor")?;
